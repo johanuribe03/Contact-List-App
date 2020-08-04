3b2e326d976168fdab1fa3748f02b2ffada7efeb
johanuribe03 <johan.uribe@gmail.com>
2020-08-04 23:50:34 +0000

    fetch working

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,22 @@
+const URL = "https://assets.breatheco.de/apis/fake/contact/agenda/my_awesome_agenda";
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
+			loadContact() {
+				fetch(URL)
+					.then(res => res.json())
+					.then(data => {
+						console.log("getContacts:", data),
+							setStore({
+								contacts: data
+							});
+					})
+					.catch(e => console.error(e));
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
